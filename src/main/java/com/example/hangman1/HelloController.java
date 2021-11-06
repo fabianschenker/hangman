d0deedd839eef308e235aaf6ddc28a1eebf15a8b
Fabian Schenker <fabian.schenker5301@gmail.com>
2021-11-06 11:32:42 +0100

    changed to jdk-16

---
+++ src/main/java/com/example/hangman1/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.hangman1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onOkButtonClick() {System.out.println("OK Button clicked!");}
+}
