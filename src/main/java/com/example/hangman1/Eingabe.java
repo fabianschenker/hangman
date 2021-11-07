3787d56c4b316c82b49bf1157357fb28a90d958f
Lara Cotti <lara.cotti@students.fhnw.ch>
2021-11-07 12:24:17 +0100

    Klasse Eingabe mit Umwandlung von Klein- zu Grossbuchstaben

--- src/main/java/com/example/hangman1/Eingabe.java
+++ src/main/java/com/example/hangman1/Eingabe.java
@@ -10,6 +10,17 @@ public class Eingabe {
 
     public Eingabe(){
         eingabe = scanner.next().charAt(0);
+
+        //Umwandlung zu Grossbuchstaben
+        eingabe = grossBuchstaben(eingabe);
+    }
+
+    public char grossBuchstaben(char e){
+        int eingabeAscii = (int)e;
+        if (eingabeAscii > 96 && eingabeAscii < 123){
+            e = (char) (eingabeAscii - 32);
+        }
+        return e;
     }
 
     public char getEingabe() {
