babe3012c3fe28fb84b7f3c0bd483e6787d7eac3
Romain Bornex <romain.bornex@bluewin.ch>
2021-11-27 15:52:23 +0100

    game engine and test v2

--- src/main/java/com/example/hangman1/Vergleich.java
+++ src/main/java/com/example/hangman1/Vergleich.java
@@ -1,6 +1,5 @@
 package com.example.hangman1;
 
-
 import java.util.Objects;
 import java.util.Vector;
 
@@ -9,24 +8,17 @@ public class Vergleich {
 
     public static boolean vergleich(char[] w, char c) {
         int counter = 0;
-        for (int i = 0; i < w.length; i++) {
-            if (Objects.equals(c, w[i])) {
-                counter++;
-            }
+        for (char test : w) {
+            if (Objects.equals(c, test)) counter++;
         }
-        if (counter > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return counter > 0;
     }
 
     public static Vector<Integer> position(char[] w, char c) {
-        Vector<Integer> position = new Vector<Integer>();
+        Vector<Integer> position = new Vector<>();
+        //classical for-loop because of use of the index
         for (int i = 0; i < w.length; i++) {
-            if (Objects.equals(c, w[i])) {
-                position.add(i);
-            }
+            if (Objects.equals(c, w[i])) position.add(i);
         }
         return position;
     }
