8565c98a0465486202bb3d338c7669f6020b66f8
Max Knauber <maxknauber@gmail.com>
2021-11-27 22:21:03 +0100

    Testklassen und Tests für Klassen "Wort" und "Vergleich" erstellt

--- src/main/java/com/example/hangman1/Vergleich.java
+++ src/main/java/com/example/hangman1/Vergleich.java
@@ -6,23 +6,19 @@ import java.util.Vector;
 
 public class Vergleich {
 
-
+//Vergleich des eingegebenen Buchstabens mit dem Array, welcher das gesuchte Wort enthält
     public static boolean vergleich(char[] w, char c) {
         int counter = 0;
-        for (int i = 0; i < w.length; i++) {
-            if (Objects.equals(c, w[i])) {
+        for (char value : w) {
+            if (Objects.equals(c, value)) {
                 counter++;
             }
         }
-        if (counter > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return counter > 0;
     }
-
+//Gibt die Position des eingegebenen Buchstabens im gesuchten Wort in einem Vektor zurück
     public static Vector<Integer> position(char[] w, char c) {
-        Vector<Integer> position = new Vector<Integer>();
+        Vector<Integer> position = new Vector<>();
         for (int i = 0; i < w.length; i++) {
             if (Objects.equals(c, w[i])) {
                 position.add(i);
--- src/main/java/com/example/hangman1/Wort.java
+++ src/main/java/com/example/hangman1/Wort.java
@@ -10,9 +10,8 @@ public class Wort {
         try{
             File file = new File("src/main/java/com/example/hangman1/wortdatenbank.txt");
             FileReader fileReader = new FileReader("src/main/java/com/example/hangman1/wortdatenbank.txt");
-
+            //Einlesen der Datei, Speicherung in einem Array aus Strings
             char[] c = new char[(int) file.length()];
-            fileReader.read(c);
             String s = new String(c);
             strArray = s.split("\r\n");
 
@@ -24,10 +23,11 @@ public class Wort {
     }
 
     public char[] selectRandomWord(){
+        //Methode zur Auswahl eines zufälligen Worts aus "strArray" von readWordFile
         String[] s = readWordFile();
         int zufall = (int) (Math.random()*(s.length-1));
         String wortString = s[zufall];
-
+        //Speicherung des zufällig ausgewählten Worts in einem char-Array
         char[] wortCharArray = new char[wortString.length()];
         for( int i=0; i<wortString.length(); i++){
             wortCharArray[i] = wortString.charAt(i);
@@ -37,6 +37,7 @@ public class Wort {
     }
 
     public static char[] buildUnderlines(char[] w){
+        //Methode zur Erstellung der Underlines um die Länge des Worts anzuzeigen
         char[] underline = new char[w.length];
         for (int i=0; i<w.length; i++){
             underline[i] = '_';
