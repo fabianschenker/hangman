c0b2fe30df4dda96ac93165b113a17d0d0e94c18
Max Knauber <maxknauber@gmail.com>
2021-11-30 15:03:59 +0100

    Kommentare korrigiert

--- src/test/java/com/example/hangman1/GameEngineTest.java
+++ src/test/java/com/example/hangman1/GameEngineTest.java
@@ -8,11 +8,11 @@ class GameEngineTest {
 
     @Test
     void test1() {
-        /*Um die Klasse test() zu testen, wir darstellen den randomWort, der "Underline Wort" der die gleiche länge des
-        randomWorts hat und ein Benutzer eingabe (Controller.ein)
-        Wir lassen die Klasse Test() mit die vorherige parameter laufen und darstellen die Positionen von die richtige A.
-        Durch die AssertTrue, sehen wir, ob die GameEngine.test() gut gelaufen ist.
-        Die Benutzung von assertAll() und lambda expression vereinfacht der Test*/
+        /*Um die Klasse test() zu testen, erzeugen wir ein randomWort um die Benutzereingabe zu simulieren
+        und lassen uns durch die entsprechende Funktion die Underlines anzeigen.
+        Wir lassen die Klasse Test() mit den vorherigen Parametern laufen und lassen uns die Positionen der A's darstellen
+        Durch AssertTrue sehen wir, ob die GameEngine.test() gut gelaufen ist.
+        Die Benutzung von assertAll() sorgt dafür, dass alle Funktionen durchlaufen werden und keine ausgelassen werden kann*/
 
         Controller.random = "JAVATESTENISTSCHWER".toCharArray();
         Controller.underlines = Wort.buildUnderlines(Controller.random);
@@ -29,24 +29,25 @@ class GameEngineTest {
     @Test
     void gewonnen() {
         /*
-        Um die Klasse gewonnen() zu testen, wir darstellen den richtige Wort durch GameEngine.richtig. Der counter hat
-        die gleich länge als GameEngine.richtig().
-        assertTrue, lass die Klasse gewonnen() mit die eingestellte Parameter. Wenn der counter = der länge der Wort ist,
-        gebe assertTrue sein "String message"
+        Um die Klasse gewonnen() zu testen, stellen wir das richtige Wort durch GameEngine.richtig dar. Der counter hat
+        die gleich Länge wie GameEngine.richtig().
+        assertTrue, testet die Klasse gewonnen() mit den eingestellten Parametern.
+        Wenn der counter == der Länge des Worts entspricht, gibt assertTrue die "String message" aus.
          */
         GameEngine.richtig = "JAVATESTENISTSCHWER";
         GameEngine.counter = GameEngine.richtig.length(); //oder 19 statt GameEngine.richtig.length()
-        assertTrue(GameEngine.gewonnen(), "GEWONNEN ==> TRUE is returned in gewonnen");
+        assertTrue(GameEngine.gewonnen(), "Die Anzahl richtiger Buchstaben stimmt nicht mit der Wortlänge überein");
     }
 
     @Test
     void doppelt() {
         /*
-        Um die Klasse doppelt() zu testen, wir darstellen den richtige Wort durch GameEngine.richtig. Die eingegebene
-        Buchstabe ist Controller.ein dargestellt und der counter ist gleich die länge des richtiges Wortes.
-        Wir lassen die klasse doppelt() mit diese Parameter laufen. Die T wurde schon bei der Parameter actual = 16
-        eingestellt, dadurch ist die eingabe 'T' eine doppeleingabe ==> der counter wurde nicht angepasst.
-         */
+        Um die Klasse doppelt() zu testen, stellen wir  das richtige Wort durch GameEngine.richtig dar. Der eingegebene
+        Buchstabe wird durch Controller.ein dargestellt und der counter ist gleich der Länge des richtiges Wortes.
+        Wir lassen die klasse doppelt() mit diesen Parametern laufen.
+        Da der Buchstabe T 3 Mal im zu suchenden Wort vorhanden ist, müsste der counter um -3 auf 16 angepasst werden.
+        Falls dies nicht klappt, wird die Error-Message geworfen.
+        */
             GameEngine.richtig = "JAVATESTENISTSCHWER";
             Controller.ein = 'T';
             GameEngine.counter = GameEngine.richtig.length();
