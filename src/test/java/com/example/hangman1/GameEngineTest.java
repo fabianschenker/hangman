509abf385519df20f1d3620d621506900f915a99
Matteo Sophys <matteosophys2811@gmail.com>
2021-11-30 14:25:09 +0100

    Darstellung die Kommentare in die Klasse GameEngineTest.java

--- src/test/java/com/example/hangman1/GameEngineTest.java
+++ src/test/java/com/example/hangman1/GameEngineTest.java
@@ -8,6 +8,12 @@ class GameEngineTest {
 
     @Test
     void test1() {
+        /*Um die Klasse test() zu testen, wir darstellen den randomWort, der "Underline Wort" der die gleiche länge des
+        randomWorts hat und ein Benutzer eingabe (Controller.ein)
+        Wir lassen die Klasse Test() mit die vorherige parameter laufen und darstellen die Positionen von die richtige A.
+        Durch die AssertTrue, sehen wir, ob die GameEngine.test() gut gelaufen ist.
+        Die Benutzung von assertAll() und lambda expression vereinfacht der Test*/
+
         Controller.random = "JAVATESTENISTSCHWER".toCharArray();
         Controller.underlines = Wort.buildUnderlines(Controller.random);
         Controller.ein = 'A';
@@ -22,13 +28,25 @@ class GameEngineTest {
 
     @Test
     void gewonnen() {
+        /*
+        Um die Klasse gewonnen() zu testen, wir darstellen den richtige Wort durch GameEngine.richtig. Der counter hat
+        die gleich länge als GameEngine.richtig().
+        assertTrue, lass die Klasse gewonnen() mit die eingestellte Parameter. Wenn der counter = der länge der Wort ist,
+        gebe assertTrue sein "String message"
+         */
         GameEngine.richtig = "JAVATESTENISTSCHWER";
         GameEngine.counter = GameEngine.richtig.length(); //oder 19 statt GameEngine.richtig.length()
-        assertTrue(GameEngine.gewonnen(), "GEWONNEN ==> TRUE is returned in gewonnen, what's activating this class gewonnen");
+        assertTrue(GameEngine.gewonnen(), "GEWONNEN ==> TRUE is returned in gewonnen");
     }
 
     @Test
     void doppelt() {
+        /*
+        Um die Klasse doppelt() zu testen, wir darstellen den richtige Wort durch GameEngine.richtig. Die eingegebene
+        Buchstabe ist Controller.ein dargestellt und der counter ist gleich die länge des richtiges Wortes.
+        Wir lassen die klasse doppelt() mit diese Parameter laufen. Die T wurde schon bei der Parameter actual = 16
+        eingestellt, dadurch ist die eingabe 'T' eine doppeleingabe ==> der counter wurde nicht angepasst.
+         */
             GameEngine.richtig = "JAVATESTENISTSCHWER";
             Controller.ein = 'T';
             GameEngine.counter = GameEngine.richtig.length();
