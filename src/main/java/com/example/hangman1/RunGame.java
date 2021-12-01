eb69451876b87057233f383052401bf9084b063a
Fabian Schenker <fabian.schenker5301@gmail.com>
2021-12-01 21:59:41 +0100

    icon für Application dazugefügt

--- src/main/java/com/example/hangman1/RunGame.java
+++ src/main/java/com/example/hangman1/RunGame.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 //Main Klasse
 public class RunGame extends Application {
@@ -13,6 +15,9 @@ public class RunGame extends Application {
     public void start(Stage mainStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RunGame.class.getResource("hangman.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 860);
+        File file = new File("src/main/resources/com/example/hangman1/icon.jpg");
+        Image icon = new Image(file.toURI().toString());
+        mainStage.getIcons().add(icon);
         mainStage.setTitle("Hangman");
         mainStage.setScene(scene);
         mainStage.show();
